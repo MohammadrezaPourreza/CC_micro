2b1c5350aaad427bcb3707d2a9d965e3a306cc8e
MohammadrezaPourreza <m1378.prz@gmail.com>
2021-03-22 11:18:32 +0430

    commit

--- CC_micro/urls.py
+++ CC_micro/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('user/',include('user_panel.urls')),
+    path('',include('user_panel.urls')),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
 ]
 
