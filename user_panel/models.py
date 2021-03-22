483fdb60875896965520510870c8171f42442e08
MohammadrezaPourreza <m1378.prz@gmail.com>
2021-03-22 12:54:46 +0430

    commit new files

--- user_panel/models.py
+++ user_panel/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class user(models.Model):
+    username = models.CharField(max_length=50,unique=True)
+    #token = models.CharField(max_length=300,unique=True)
+    name = models.CharField(max_length=20)
+    family_name = models.CharField(max_length=30)
+    phone_number = models.BigIntegerField()
+    email = models.EmailField(max_length=254)
+
