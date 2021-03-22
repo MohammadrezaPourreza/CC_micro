e76500e4eb9b9762304c8a1b7cf2e51693d2d527
MohammadrezaPourreza <m1378.prz@gmail.com>
2021-03-22 13:21:12 +0430

    models added

--- doctor_panel/models.py
+++ doctor_panel/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django import forms
 
+CITY_CHOICES =(
+    ("TEHRAN", "tehran"),
+    ("MASHHAD", "mashhad"),
+)
 # Create your models here.
+class Doctor(models.Model):
+    username = models.CharField(max_length=50,unique=True)
+    #token = models.CharField(max_length=300,unique=True)
+    name = models.CharField(max_length=20)
+    family_name = models.CharField(max_length=30)
+    phone_number = models.BigIntegerField()
+    medical_association_id = models.IntegerField(unique=True)
+    clinic_address = models.TextField()
+    city = forms.ChoiceField(choices=CITY_CHOICES)
+    medical_expertise = models.TextField()
+    off_days = models.TextField(null=True)
+    opening_time = models.TextField(null=True,default="8:00AM")
+    closing_time = models.TextField(null=True,default="12:00PM")
+
+class FreeTimes(models.Model):
+    doctor_username = models.CharField(max_length=50)
+    date = models.DateField()
+    time = models.TimeField()
+    duration = models.IntegerField()
+
--- user_panel/models.py
+++ user_panel/models.py
@@ -1,12 +1,31 @@
 from django.db import models
+import doctor_panel.models
 
 # Create your models here.
 
-class user(models.Model):
+class User(models.Model):
     username = models.CharField(max_length=50,unique=True)
     #token = models.CharField(max_length=300,unique=True)
     name = models.CharField(max_length=20)
     family_name = models.CharField(max_length=30)
     phone_number = models.BigIntegerField()
-    email = models.EmailField(max_length=254)
+    email = models.EmailField(max_length=254,null=True)
+
+class FavoriteDoctor(models.Model):
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    doctor_id = models.ForeignKey(doctor_panel.models.Doctor,on_delete=models.CASCADE)
+    add_date = models.DateField()
+
+class Comments(models.Model):
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    doctor_id = models.ForeignKey(doctor_panel.models.Doctor,on_delete=models.CASCADE)
+    commment = models.TextField()
+    commment_date = models.DateTimeField()
+
+class Reservation(models.Model):
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    doctor_id = models.ForeignKey(doctor_panel.models.Doctor,on_delete=models.CASCADE)
+    submit_date = models.DateTimeField()
+    reservation_date = models.DateTimeField()
+    is_payed = models.BooleanField(default=False)
 
