35e5cda3a5ba1c45cd67441e511f124c9bd292cf
Reza <reza.barzgar99@gmail.com>
2021-03-23 11:45:59 +0430

    adding serializers

--- user_panel/urls.py
+++ user_panel/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'user_panel'
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('', views.index, name='index'),
+    path('add-user/', views.user_list, name='user_list'),
 ]
--- user_panel/views.py
+++ user_panel/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import Http404
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from user_panel.models import *
+from user_panel.serializers import *
+
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello reza")
+
+@csrf_exempt
+def user_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = UserSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
