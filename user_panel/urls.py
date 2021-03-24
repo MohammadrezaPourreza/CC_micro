54b8af157757a68c1587eb95e6bb5f45d590ee1b
Reza <reza.barzgar99@gmail.com>
2021-03-24 19:17:44 +0430

    adding api (user-edit/)

--- user_panel/urls.py
+++ user_panel/urls.py
@@ -5,7 +5,9 @@ app_name = 'user_panel'
 urlpatterns = [
     path('add-user/', views.user_list, name='user_list'),
     path('add-comment/', views.add_comment, name='add_comment'),
-    path('get-doctor-by-city/',views.get_doctor_list_by_city,name='get_doctor_list_by_city'),
-    path('get-doctor-freeTimes/',views.get_doctor_freetimes,name='get_doctor_freetimes'),
-    path('find-doctor-by-name-or-medicalId/',views.find_doctor__by_name_or_medicalId,name='find_doctor__by_name_or_medicalId')
+    path('get-doctor-by-city/', views.get_doctor_list_by_city, name='get_doctor_list_by_city'),
+    path('get-doctor-freeTimes/', views.get_doctor_freetimes, name='get_doctor_freetimes'),
+    path('find-doctor-by-name-or-medicalId/', views.find_doctor_by_name_or_medicalId,
+         name='find_doctor_by_name_or_medicalId'),
+    path('edit-user/', views.edit_user, name='edit_user'),
 ]
--- user_panel/views.py
+++ user_panel/views.py
@@ -8,6 +8,7 @@ from user_panel.models import *
 import json
 import os
 from user_panel.serializers import *
+import traceback
 
 
 # Create your views here.
@@ -30,6 +31,7 @@ def user_list(request):
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
 
+
 @csrf_exempt
 def add_comment(request):
     if request.method == 'POST':
@@ -42,11 +44,12 @@ def add_comment(request):
     else:
         return JsonResponse(status=400)
 
+
 @csrf_exempt
 def get_doctor_list_by_city(request):
     if request.method == 'POST':
         city = request.POST['city']
-        #acctualy we have to call doctor_panel api
+        # acctualy we have to call doctor_panel api
         dir_path = os.path.dirname(os.path.realpath(__file__))
         json_data = open(dir_path + '/' + 'doctors.json')
         data1 = json.load(json_data)
@@ -56,7 +59,7 @@ def get_doctor_list_by_city(request):
                 doctors_list.append(data)
         json_data.close()
         serializer = DoctorSerializer(doctors_list, many=True)
-        return JsonResponse(serializer.data,safe=False,status=201)
+        return JsonResponse(serializer.data, safe=False, status=201)
     else:
         return HttpResponse(status=400)
 
@@ -65,7 +68,7 @@ def get_doctor_list_by_city(request):
 def get_doctor_freetimes(request):
     if request.method == 'POST':
         doctor = request.POST['doctor_username']
-        #acctualy we have to call doctor_panel api
+        # acctualy we have to call doctor_panel api
         dir_path = os.path.dirname(os.path.realpath(__file__))
         json_data = open(dir_path + '/' + 'freeTimes.json')
         data1 = json.load(json_data)
@@ -75,16 +78,17 @@ def get_doctor_freetimes(request):
                 freeTimes_list.append(data)
         json_data.close()
         serializer = FreeTimesSerializer(freeTimes_list, many=True)
-        return JsonResponse(serializer.data,safe=False,status=201)
+        return JsonResponse(serializer.data, safe=False, status=201)
     else:
         return HttpResponse(status=400)
 
+
 @csrf_exempt
-def find_doctor__by_name_or_medicalId(request):
+def find_doctor_by_name_or_medicalId(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         medical_association_id = request.POST.get('medical_association_id')
-        #acctualy we have to call doctor_panel api
+        # actually we have to call doctor_panel api
         dir_path = os.path.dirname(os.path.realpath(__file__))
         json_data = open(dir_path + '/' + 'doctors.json')
         data1 = json.load(json_data)
@@ -100,6 +104,38 @@ def find_doctor__by_name_or_medicalId(request):
                         doctors_list.append(data)
         json_data.close()
         serializer = DoctorSerializer(doctors_list, many=True)
-        return JsonResponse(serializer.data,safe=False,status=201)
+        return JsonResponse(serializer.data, safe=False, status=201)
+    else:
+        return HttpResponse(status=400)
+
+
+@csrf_exempt
+def edit_user(request):
+    # if you wanna edit, you must send all parameters of the user model
+    if request.method == 'GET':
+        username = request.GET['username']
+        user = User.objects.get(username=username)
+        serializer = UserSerializer(user, many=False)
+        return JsonResponse(serializer.data, safe=False)
+
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        print(data)
+        user = User.objects.get(username=data['username'])
+        try:
+            if data['name']:
+                user.name = data['name']
+            if data['family_name']:
+                user.family_name = data['family_name']
+            if data['phone_number']:
+                user.phone_number = data['phone_number']
+            if data['email']:
+                user.email = data['email']
+            user.save()
+            serializer = UserSerializer(user, many=False)
+            return JsonResponse(serializer.data, safe=False)
+        except Exception:
+            traceback.print_exc()
+            return HttpResponse(status=400)
     else:
         return HttpResponse(status=400)
